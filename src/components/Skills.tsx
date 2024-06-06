78020aa19d566181ddc185e2af2538babf871bbd
Samuel De los Santos Ruiz <samuel.delossantos@fiscalianl.gob.mx>
2024-06-06 14:24:53 -0600

    Se agregan mas habilidades, se agrega texto descriptivo de la habilidad, y se modifica el w-maz por el tamaño de la pantalla

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,9 +1,12 @@
-import { AiOutlineJavaScript } from "react-icons/ai";
 import {
   SiDotnet,
   SiTypescript,
   SiMicrosoftsqlserver,
   SiTailwindcss,
+  SiHtml5,
+  SiBootstrap,
+  SiJavascript,
+  SiCss3
 } from "react-icons/si";
 import { FaReact } from "react-icons/fa6";
 import { useRef } from "react";
@@ -14,7 +17,7 @@ import {
   useTransform,
   useMotionValue,
   useVelocity,
-  useAnimationFrame
+  useAnimationFrame,
 } from "framer-motion";
 import { wrap } from "@motionone/utils";
 
@@ -29,10 +32,10 @@ function ParallaxText({ children, baseVelocity = 100 }: ParallaxProps) {
   const scrollVelocity = useVelocity(scrollY);
   const smoothVelocity = useSpring(scrollVelocity, {
     damping: 50,
-    stiffness: 400
+    stiffness: 400,
   });
   const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
-    clamp: false
+    clamp: false,
   });
 
   /**
@@ -82,49 +85,69 @@ function ParallaxText({ children, baseVelocity = 100 }: ParallaxProps) {
 
 const Skills = () => {
   return (
-    <div className="mx-3 md:mx-4 lg:mx-5 xl:mx-auto mt-20 w-full">
+    <div className="mx-3 md:mx-4 lg:mx-5 xl:mx-auto mt-20">
       <h2 className="text-bluechill-50 text-center text-4xl font-bold mb-10">
         Skills
       </h2>
 
-      <ParallaxText baseVelocity={3}>
-      <div className="flex items-center md:justify-center gap-7 mr-7">
-        <div
-          className="rounded-lg p-2 backdrop-blur-xl backdrop-brightness-75 bg-white/20"
-        >
-          <SiDotnet className="text-violet-500 text-9xl" />
+      <ParallaxText baseVelocity={-2}>
+        <div className="flex items-center md:justify-center gap-7 mr-7">
+          <div className="group/skill rounded-lg p-2 backdrop-blur-xl backdrop-brightness-75 bg-white/20 hover:bg-white/10 relative place-content-center">
+            <SiDotnet className="text-violet-500 text-9xl group-hover/skill:blur" />
+            <p className="invisible group-hover/skill:visible absolute inset-0 text-center content-center text-white font-bold text-xl">
+              .NET
+            </p>
+          </div>
+          <div className="group/skill rounded-lg p-2 backdrop-blur-xl backdrop-brightness-75 bg-white/20 hover:bg-white/10 relative place-content-center">
+            <SiMicrosoftsqlserver className="text-yellow-600 text-9xl group-hover/skill:blur" />
+            <p className="invisible group-hover/skill:visible absolute inset-0 text-center content-center text-white font-bold text-xl">
+              SQL Server
+            </p>
+          </div>
+          <div className="group/skill rounded-lg p-2 backdrop-blur-xl backdrop-brightness-75 bg-white/20 hover:bg-white/10 relative place-content-center">
+            <FaReact className="text-blue-300 text-9xl group-hover/skill:blur" />
+            <p className="invisible group-hover/skill:visible absolute inset-0 text-center content-center text-white font-bold text-xl">
+              React.js
+            </p>
+          </div>
+          <div className="group/skill rounded-lg p-2 backdrop-blur-xl backdrop-brightness-75 bg-white/20 hover:bg-white/10 relative place-content-center">
+            <SiTailwindcss className="text-sky-700 text-9xl group-hover/skill:blur" />
+            <p className="invisible group-hover/skill:visible absolute inset-0 text-center content-center text-white font-bold text-xl">
+              Tailwind CSS
+            </p>
+          </div>
+          <div className="group/skill rounded-lg p-2 backdrop-blur-xl backdrop-brightness-75 bg-white/20 hover:bg-white/10 relative place-content-center">
+            <SiHtml5 className=" text-red-500 text-9xl group-hover/skill:blur" />
+            <p className="invisible group-hover/skill:visible absolute inset-0 text-center content-center text-white font-bold text-xl">
+              HTML
+            </p>
+          </div>
+          <div className="group/skill rounded-lg p-2 backdrop-blur-xl backdrop-brightness-75 bg-white/20 hover:bg-white/10 relative place-content-center">
+            <SiCss3 className=" text-sky-500 text-9xl group-hover/skill:blur" />
+            <p className="invisible group-hover/skill:visible absolute inset-0 text-center content-center text-white font-bold text-xl">
+              CSS
+            </p>
+          </div>
+          <div className="group/skill rounded-lg p-2 backdrop-blur-xl backdrop-brightness-75 bg-white/20 hover:bg-white/10 relative place-content-center">
+            <SiBootstrap className=" text-violet-700 text-9xl group-hover/skill:blur" />
+            <p className="invisible group-hover/skill:visible absolute inset-0 text-center content-center text-white font-bold text-xl">
+              Bootstrap
+            </p>
+          </div>
+          <div className="group/skill rounded-lg p-2 backdrop-blur-xl backdrop-brightness-75 bg-white/20 hover:bg-white/10 relative place-content-center">
+            <SiJavascript className="text-yellow-400 text-9xl group-hover/skill:blur" />
+            <p className="invisible group-hover/skill:visible absolute inset-0 text-center content-center text-white font-bold text-xl">
+              JavaScript
+            </p>
+          </div>
+          <div className="group/skill rounded-lg p-2 backdrop-blur-xl backdrop-brightness-75 bg-white/20 hover:bg-white/10 relative place-content-center">
+            <SiTypescript className="text-blue-500 text-9xl group-hover/skill:blur" />
+            <p className="invisible group-hover/skill:visible absolute inset-0 text-center content-center text-white font-bold text-xl">
+              TypeScript
+            </p>
+          </div>
         </div>
-        <div
-          className="rounded-lg p-2 backdrop-blur-xl backdrop-brightness-75 bg-white/20"
-        >
-          <SiMicrosoftsqlserver className="text-yellow-600 text-9xl" />
-        </div>
-        <div
-          className="rounded-lg p-2 backdrop-blur-xl backdrop-brightness-75 bg-white/20"
-        >
-          <AiOutlineJavaScript className="text-yellow-400 text-9xl" />
-        </div>
-        <div
-          className="rounded-lg p-2 backdrop-blur-xl backdrop-brightness-75 bg-white/20"
-        >
-          <SiTypescript className="text-blue-500 text-9xl" />
-        </div>
-        <div
-          className="rounded-lg p-2 backdrop-blur-xl backdrop-brightness-75 bg-white/20"
-        >
-          <FaReact className="text-blue-300 text-9xl" />
-        </div>
-        <div
-          className="rounded-lg  p-2 backdrop-blur-xl backdrop-brightness-75 bg-white/20"
-        >
-          <SiTailwindcss className="text-sky-700 text-9xl" />
-        </div>
-      </div>
       </ParallaxText>
-
-
-
-
 
       {/* <div className="flex flex-col md:flex-row items-center md:justify-center gap-5">
         <motion.div
@@ -172,7 +195,6 @@ const Skills = () => {
       </div> */}
       {/* <ParallaxText baseVelocity={-3}>Framer Motion</ParallaxText>
       <ParallaxText baseVelocity={3}>Scroll velocity</ParallaxText> */}
-     
     </div>
   );
 };
